b44aadf81f219ed7db2667d65625da661e2a5a8f
shivam gupta <guptashivam3935@gmail.com>
2024-10-13 16:22:35 +0530

    adding postfrom folder

--- src/components/index.js
+++ src/components/index.js
@@ -5,6 +5,8 @@ import LogoutBtn from "./Header/LogoutBtn";
 import Logo from "./Logo";
 import Input from './Input'
 import Button from './Button'
+import RTE from "./RTE";
+import Select from './Select'
 
 export {
     Header,
@@ -13,5 +15,7 @@ export {
     LogoutBtn,
     Logo,
     Input,
-    Button
+    Button,
+    RTE,
+    Select
 }
